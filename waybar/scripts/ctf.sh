d479d03774a7684d379dead235624829d8cc32d4
tico <sebastianmihut@gmail.com>
2024-07-08 22:15:25 +0200

    ii

--- waybar/scripts/ctf.sh
+++ waybar/scripts/ctf.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
+ovpn=$(pgrep openvpn)
+
+if [ $ovpn ]
+then 
+	echo "Openvpn is already running: $ovpn"
+else
+	htb
+fi
+
 echo -n "MACHINE NAME: "
 read -r name
-mkdir /home/tico/CTF/HTB/$name
 
+if [ -e /home/tico/CTF/HTB/$name ]
+then
+	echo 'Directory already existing'
+else
+	mkdir /home/tico/CTF/HTB/$name
+fi
 echo -n "MACHINE IP: "
 read -r ip
 
 echo $ip > ~/CTF/.IP
+export ip=$ip
 cd /home/tico/CTF/HTB/$name
 tmux
--- waybar/scripts/vpn.sh
+++ waybar/scripts/vpn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 MachineIP=$(cat ~/CTF/.IP | tail -n 1)
-tun0_ip=$(ip a | grep -oP 'inet \K[\d.]+(?=.*tun0)') && echo "VPN: $tun0_ip | CTF: $MachineIP"  || echo 'VPN:  '
+tun0_ip=$(ip a | grep -oP 'inet \K[\d.]+(?=.*tun0)') && echo "󰞇 : $MachineIP"  || echo '󰞇 : No Target'
