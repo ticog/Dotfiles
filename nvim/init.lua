3289bc7b32a4529d0814abc3746c14e870b98142
tico <sebastianmihut@gmail.com>
2024-07-12 19:50:05 +0200

    neovim add

---
+++ nvim/init.lua
@@ -0,0 +1,20 @@
+require("keymaps")
+require("options")
+
+-- [[ Basic Autocommands ]]
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- [[ "lazy.nvim" plugin manager ]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
