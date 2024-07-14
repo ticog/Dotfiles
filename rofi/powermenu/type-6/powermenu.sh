4c6f55ae5b0c3eae981273656b9022d302abb02d
Sebastian Masis <sebastianmihut@gmail.ch>
2024-07-14 15:59:17 +0200

    B laptop config
    A
    A
    B
    B
    added

--- rofi/powermenu/type-6/powermenu.sh
+++ rofi/powermenu/type-6/powermenu.sh
@@ -102,7 +102,11 @@ case ${chosen} in
 		run_cmd --hibernate
         ;;
     $lock)
-		hyprctl dispatch exit 0
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
         ;;
     $suspend)
 		run_cmd --suspend
